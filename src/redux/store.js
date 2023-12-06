f221ef75491947db2c61560fe7a3d3966c318791
yurii-corssa <yurii.corssa@gmail.com>
2023-12-06 20:58:29 +0100

    Create Redux Store

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+export const store = configureStore({
+  reducer: {
+    adverts: null,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
