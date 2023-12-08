d0b0bf2a1f33d5f61ad893ddf9b94e5f3cb8552c
yurii-corssa <yurii.corssa@gmail.com>
2023-12-08 12:40:40 +0100

    Create global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 import SharedLayout from "./SharedLayout/SharedLayout";
+import { GlobalStyles } from "./GlobalStyles";
 
 const HomePage = lazy(() => import("pages/HomePage/HomePage"));
 const CatalogPage = lazy(() => import("pages/CatalogPage/CatalogPage"));
@@ -8,13 +9,16 @@ const FavoritesPage = lazy(() => import("pages/FavoritesPage/FavoritesPage"));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Route>
-    </Routes>
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,59 @@
+import { createGlobalStyle, styled } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --accent: #3470ff;
+    --accentActive: #0B44CD;
+    --white: #FFF;
+    --black: #121417;
+    --transition: 300ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Manrope', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: var(--white);
+    color: var(--black);
+  }
+
+  ul, ol {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  img {
+    display: block;
+  }
+
+`;
+
+export const ButtonStyled = styled.button`
+  display: flex;
+  padding: 12px 99px;
+  justify-content: center;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  border: none;
+  border-radius: 12px;
+  color: var(--white);
+  background-color: var(--accent);
+  transition: background-color var(--transition);
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: var(--accentActive);
+  }
+`;
