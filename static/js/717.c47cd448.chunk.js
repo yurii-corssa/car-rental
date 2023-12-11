4b11d7ee512bd4275ced737ad9a9c0e173628988
yurii-corssa <122684054+yurii-corssa@users.noreply.github.com>
2023-12-11 12:24:36 +0000

    Deploying to gh-pages from @ yurii-corssa/car-rental@42457a179252827bf5edcc33f71f79ccf66d0d93 🚀

--- static/js/717.c47cd448.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcar_rental=self.webpackChunkcar_rental||[]).push([[717],{717:(a,e,n)=>{n.r(e),n.d(e,{default:()=>l});var r,c=n(168);const s=n(867).zo.div(r||(r=(0,c.Z)(["\n  //\n"])));var t=n(184);const l=()=>(0,t.jsx)(s,{children:"FAVORITES PAGE"})}}]);
-//# sourceMappingURL=717.c47cd448.chunk.js.map
